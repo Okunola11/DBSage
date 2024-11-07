42292c8595e932bf0900ec4be19132e55dcc4b44
okunola <abdayoade@gmail.com>
2024-11-07 06:38:00 +0100

    refactor: removed other schemas, they are no longer in use
    OAuthToken schema defines the requeset data for the google login
    endpoint.

--- db_sage/app/v1/schemas/google_oauth.py
+++ db_sage/app/v1/schemas/google_oauth.py
@@ -1,33 +1,4 @@
-from datetime import datetime
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 
-class UserData(BaseModel):
-    """
-    Schema response for validated google login
-    """
-    id: str
-    first_name: str
-    last_name: str 
-    email: EmailStr
-    created_at: datetime
-
-    class Config:
-        from_attributes = True
-
-class Tokens(BaseModel):
-    """
-    Schema for generated tokens response
-    """
-    access_token: str
-    refresh_token: str
-    token_type: str
-
-class StatusResponse(BaseModel):
-    """
-    Response schema for the end user 
-    """
-    message: str
-    status: str
-    status_code: int = 200
-    tokens: Tokens
-    user: UserData
+class OAuthToken(BaseModel):
+    id_token: str
