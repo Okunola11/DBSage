388351fb9e4219f8ef746f4fe129f0ab533b9f65
Okunola11 <abdayoade.com>
2025-01-02 11:59:17 +0100

    refactor: format file with python black formatter

--- db_sage/app/core/config/security.py
+++ db_sage/app/core/config/security.py
@@ -10,10 +10,12 @@ from db_sage.app.utils.logger import logger
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
+
 def hash_password(password):
     """Function to hash password"""
 
     return pwd_context.hash(password)
+
 
 def verify_password(plain_password: str, hashed_password: str):
     """Verifies a hashed password
@@ -28,6 +30,7 @@ def verify_password(plain_password: str, hashed_password: str):
 
     return pwd_context.verify(plain_password, hashed_password)
 
+
 def str_encode(string: str) -> str:
     """Encodes a string
 
@@ -38,7 +41,8 @@ def str_encode(string: str) -> str:
         str: the encoded string
     """
 
-    return base64.b85encode(string.encode('ascii')).decode('ascii')
+    return base64.b85encode(string.encode("ascii")).decode("ascii")
+
 
 def str_decode(string: str) -> str:
     """Decodes a string
@@ -50,7 +54,8 @@ def str_decode(string: str) -> str:
         str: the decoded string
     """
 
-    return base64.b85decode(string.encode('ascii')).decode('ascii')
+    return base64.b85decode(string.encode("ascii")).decode("ascii")
+
 
 def generate_token(payload: dict, secret: str, algo: str, expiry: timedelta):
     """Generated a jwt token
@@ -68,6 +73,7 @@ def generate_token(payload: dict, secret: str, algo: str, expiry: timedelta):
     expire = datetime.utcnow() + expiry
     payload.update({"exp": expire})
     return jwt.encode(payload, secret, algorithm=algo)
+
 
 def get_token_payload(token: str, secret: str, algo: str):
     """Retrieves a token payload
@@ -88,6 +94,7 @@ def get_token_payload(token: str, secret: str, algo: str):
         payload = None
     return payload
 
+
 async def get_token_user(token: str, db: Session):
     """Retrieves the user associated with a token
 
@@ -100,20 +107,26 @@ async def get_token_user(token: str, db: Session):
     """
     payload = get_token_payload(token, settings.SECRET_KEY, settings.ALGORITHM)
     if payload:
-        user_token_id = str_decode(payload.get('r'))
-        user_id = str_decode(payload.get('sub'))
-        access_key = payload.get('a')
+        user_token_id = str_decode(payload.get("r"))
+        user_id = str_decode(payload.get("sub"))
+        access_key = payload.get("a")
 
-        user_token = db.query(UserToken).options(joinedload(UserToken.user)).filter(
-            UserToken.access_key == access_key,
-            UserToken.id == user_token_id,
-            UserToken.user_id == user_id,
-            UserToken.expires_at > datetime.utcnow()
-        ).first()
+        user_token = (
+            db.query(UserToken)
+            .options(joinedload(UserToken.user))
+            .filter(
+                UserToken.access_key == access_key,
+                UserToken.id == user_token_id,
+                UserToken.user_id == user_id,
+                UserToken.expires_at > datetime.utcnow(),
+            )
+            .first()
+        )
 
         if user_token:
             return user_token.user
     return None
+
 
 async def load_user(email: str, db: Session):
     """Gets a user by email
@@ -127,9 +140,10 @@ async def load_user(email: str, db: Session):
     """
 
     from db_sage.app.v1.models.user import User
+
     try:
-        user = db.query(User).filter_by(email = email).first()
+        user = db.query(User).filter_by(email=email).first()
     except Exception as user_exec:
         logger.info(f"User Not Found, Email: {email}")
         user = None
-    return user    
+    return user
