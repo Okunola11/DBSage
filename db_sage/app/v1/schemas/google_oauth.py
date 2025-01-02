56050b7485dbe81001eb1db64f02bc209b4edc30
Okunola11 <abdayoade.com>
2025-01-02 14:46:39 +0100

    chore: add google login token schemas

--- db_sage/app/v1/schemas/google_oauth.py
+++ db_sage/app/v1/schemas/google_oauth.py
@@ -1,4 +1,28 @@
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+
+from db_sage.app.v1.responses.user import UserResponseData
+
 
 class OAuthToken(BaseModel):
     id_token: str
+
+
+class GoogleLoginTokenRequest(BaseModel):
+    """Request schema for google login token"""
+
+    token: str = Field(examples=["ljjf0939jlfjkfj"])
+
+
+class GoogleLoginTokenResponse(BaseModel):
+    """
+    Response model for token response after google authorization
+    """
+
+    success: bool = Field(default=True, examples=[True])
+    status_code: int = Field(default=200, examples=[200])
+    message: str = Field(
+        default="Google login successful", examples=["Google login successful"]
+    )
+    access_token: str = Field(examples=["some_jwt_token"])
+    expires_in: int = Field(examples=[60])
+    data: UserResponseData
